591e6bf63a9def4a8b281aafd485a624fe1c09cc
MrMoore <pmoore@heckgrammar.co.uk>
2024-06-20 12:47:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(x+x);
+        }
+    }
+}
