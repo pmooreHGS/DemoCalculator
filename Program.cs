25728a5e1bb76061b8e380f475993dc048637a84
MrMoore <pmoore@heckgrammar.co.uk>
2024-06-27 13:32:21 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,13 @@ namespace DemoCalculator
         static void Main(string[] args)
         {
             int x = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(x + x);
-            Console.WriteLine();
+            Console.WriteLine(Square(x));
+
+        }
+        static int Square(int num1)
+        {
+            return num1 * num1;
         }
     }
+
 }
